14da9eded640aaf36eb1dd1e027ecd5f61c053c6
RishiTiwari-18 <153971668+RishiTiwari-18@users.noreply.github.com>
2023-12-22 22:07:10 +0530

    completed

---
+++ whatever/script.js
@@ -0,0 +1,77 @@
+gsap.to(".page1",{
+    y:"-56%",
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.from(".character",{
+    y:"-25%",
+    duration:1.5,
+    delay:1,
+    scale:1.2,
+    ease: "power3.inOut",
+    filter:" grayscale(100%)"
+   
+})
+
+
+
+gsap.to(".pie",{
+    y:-560,
+    rotate:140.949,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.from(".background",{
+    scale:0,
+    duration:1.5,
+    delay:1,
+    ease: "power4.inOut",
+})
+
+gsap.from(".menu",{
+    scale:0,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.from(".line",{
+    scale:0,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.from(".text2",{
+    scale:3,
+    opacity:0,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.from(".top-icon",{
+    y:40,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.to(".text p",{
+    y:-450,
+    duration:1.5,
+    delay:1,
+    ease: "power3.inOut",
+})
+
+gsap.to(".text h1",{
+    y:-150,
+    duration:2,
+    delay:1,
+    ease: "power3.inOut",
+})
+
